5f9748ff0efac92bd17e41f783ed16ca6c69e886
wighawag <wighawag@gmail.com>
2015-12-08 20:20:23 +0000

    use khafile.js

--- tutorial01/Sources/Empty.hx
+++ tutorial01/Sources/Empty.hx
@@ -1,18 +1,17 @@
 package;
 
-import kha.Game;
 import kha.Framebuffer;
 import kha.Color;
 
 using Khage;
 
-class Empty extends Game {
+class Empty {
 
 	public function new() {
-		super("Empty");
+		
 	}
 
-	override public function render(frame:Framebuffer) {
+	public function render(frame:Framebuffer) {
 
 		frame.usingG4({
 				g4.clear(Color.Black);
